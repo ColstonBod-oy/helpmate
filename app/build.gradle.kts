b4a60c8d2ba81cbac9e3d0ef830d68b9843f4ca1
Colston <bodoycolstond@gmail.com>
2024-05-16 23:18:13 +0800

    updated theme to use material 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
     
     defaultConfig {
         minSdk = 19
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         multiDexEnabled = true
@@ -34,6 +34,7 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    namespace = "com.colston.helpmate"
 }
 
 dependencies {
@@ -43,5 +44,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("com.google.android.gms:play-services-location:21.2.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
 
