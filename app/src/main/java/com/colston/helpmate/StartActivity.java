351d2f2a851be2c458a049bcbadb1cfb18f49497
Colston <bodoycolstond@gmail.com>
2024-07-10 20:26:50 +0800

    added license headers part 3

--- app/src/main/java/com/colston/helpmate/StartActivity.java
+++ app/src/main/java/com/colston/helpmate/StartActivity.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2023 Colston Bod-oy
+ *
+ * Copyright 2019 Jeremy Walker and contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.colston.helpmate;
 
 import android.Manifest;
@@ -11,7 +28,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
@@ -19,7 +35,6 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
@@ -28,149 +43,162 @@ import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
-
 import java.security.SecureRandom;
 
 public class StartActivity extends AppCompatActivity {
-    private static final int LOCATION_PERMISSIONS_REQUEST_CODE = 0;
-    private static final int BLUETOOTH_PERMISSIONS_REQUEST_CODE = 1;
-    private static final int RECORD_AUDIO_PERMISSION_REQUEST_CODE = 2;
-    private static final int REQUEST_CHECK_SETTINGS = 3;
-    private static final String ALPHA_NUMERIC_STRING = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    @RequiresApi(api = Build.VERSION_CODES.S)
-    private static final String[] ANDROID_12_BLE_PERMISSIONS = new String[]{android.Manifest.permission.BLUETOOTH_ADVERTISE,
-            android.Manifest.permission.BLUETOOTH_CONNECT,
-            Manifest.permission.BLUETOOTH_SCAN};
+  private static final int LOCATION_PERMISSIONS_REQUEST_CODE = 0;
+  private static final int BLUETOOTH_PERMISSIONS_REQUEST_CODE = 1;
+  private static final int RECORD_AUDIO_PERMISSION_REQUEST_CODE = 2;
+  private static final int REQUEST_CHECK_SETTINGS = 3;
+  private static final String ALPHA_NUMERIC_STRING =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+  @RequiresApi(api = Build.VERSION_CODES.S)
+  private static final String[] ANDROID_12_BLE_PERMISSIONS =
+      new String[] {
+        android.Manifest.permission.BLUETOOTH_ADVERTISE,
+        android.Manifest.permission.BLUETOOTH_CONNECT,
+        Manifest.permission.BLUETOOTH_SCAN
+      };
 
-        // Inflate custom action bar layout
-        View customActionBar = getLayoutInflater().inflate(R.layout.start_actionbar, null);
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_start);
 
-        // Set custom view for ActionBar
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setCustomView(customActionBar);
+    // Inflate custom action bar layout
+    View customActionBar = getLayoutInflater().inflate(R.layout.start_actionbar, null);
 
-        Button btn_continue = findViewById(R.id.btn_continue);
-        final EditText et_nodeName = findViewById(R.id.et_username);
-        et_nodeName.setText(generateNodeName(10));
+    // Set custom view for ActionBar
+    getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+    getSupportActionBar().setCustomView(customActionBar);
 
-        // Prompt the user to grant permissions
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_COARSE_LOCATION},
-                    LOCATION_PERMISSIONS_REQUEST_CODE);
-        } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADVERTISE)
-                != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT)
-                        != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN)
-                        != PackageManager.PERMISSION_GRANTED
-        ) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                ActivityCompat.requestPermissions(this,
-                        ANDROID_12_BLE_PERMISSIONS,
-                        BLUETOOTH_PERMISSIONS_REQUEST_CODE);
+    Button btn_continue = findViewById(R.id.btn_continue);
+    final EditText et_nodeName = findViewById(R.id.et_username);
+    et_nodeName.setText(generateNodeName(10));
+
+    // Prompt the user to grant permissions
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+      ActivityCompat.requestPermissions(
+          this,
+          new String[] {
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
+          },
+          LOCATION_PERMISSIONS_REQUEST_CODE);
+    } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADVERTISE)
+            != PackageManager.PERMISSION_GRANTED
+        || ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT)
+            != PackageManager.PERMISSION_GRANTED
+        || ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN)
+            != PackageManager.PERMISSION_GRANTED) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        ActivityCompat.requestPermissions(
+            this, ANDROID_12_BLE_PERMISSIONS, BLUETOOTH_PERMISSIONS_REQUEST_CODE);
+      }
+    } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+        != PackageManager.PERMISSION_GRANTED) {
+      ActivityCompat.requestPermissions(
+          this,
+          new String[] {Manifest.permission.RECORD_AUDIO},
+          RECORD_AUDIO_PERMISSION_REQUEST_CODE);
+    }
+
+    // Set up a location request
+    LocationRequest locationRequest =
+        new LocationRequest.Builder(LocationRequest.PRIORITY_HIGH_ACCURACY, 10000).build();
+
+    // Get current location settings
+    LocationSettingsRequest.Builder builder =
+        new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+    SettingsClient client = LocationServices.getSettingsClient(this);
+    Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+    // Prompt the user to change the location settings
+    // and enable GPS system settings to allow device discovery
+    task.addOnFailureListener(
+        this,
+        new OnFailureListener() {
+          @Override
+          public void onFailure(@NonNull Exception e) {
+            if (e instanceof ResolvableApiException) {
+              // Location settings are not satisfied, but this can be fixed
+              // by showing the user a dialog.
+              try {
+                // Show the dialog by calling startResolutionForResult(),
+                // and check the result in onActivityResult().
+                ResolvableApiException resolvable = (ResolvableApiException) e;
+                resolvable.startResolutionForResult(StartActivity.this, REQUEST_CHECK_SETTINGS);
+              } catch (IntentSender.SendIntentException sendEx) {
+                // Ignore the error.
+              }
             }
-        } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.RECORD_AUDIO},
-                    RECORD_AUDIO_PERMISSION_REQUEST_CODE);
-        }
-
-        // Set up a location request
-        LocationRequest locationRequest = new LocationRequest.Builder(LocationRequest.PRIORITY_HIGH_ACCURACY, 10000).build();
-
-        // Get current location settings
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest);
-        SettingsClient client = LocationServices.getSettingsClient(this);
-        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
-
-        // Prompt the user to change the location settings
-        // and enable GPS system settings to allow device discovery
-        task.addOnFailureListener(this, new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                if (e instanceof ResolvableApiException) {
-                    // Location settings are not satisfied, but this can be fixed
-                    // by showing the user a dialog.
-                    try {
-                        // Show the dialog by calling startResolutionForResult(),
-                        // and check the result in onActivityResult().
-                        ResolvableApiException resolvable = (ResolvableApiException) e;
-                        resolvable.startResolutionForResult(StartActivity.this,
-                                REQUEST_CHECK_SETTINGS);
-                    } catch (IntentSender.SendIntentException sendEx) {
-                        // Ignore the error.
-                    }
-                }
-            }
+          }
         });
 
-        btn_continue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String nodName = et_nodeName.getText().toString();
-                if (TextUtils.isEmpty(nodName) || TextUtils.isEmpty(nodName.trim())) {
-                    et_nodeName.setError(getString(R.string.err_invalidNodeName));
-                    return;
-                }
-                nodName = nodName.trim();
-                PeerDetails.getInstance().setPeerAddress(nodName);
-                startActivity(new Intent(StartActivity.this, MainActivity.class));
+    btn_continue.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            String nodName = et_nodeName.getText().toString();
+            if (TextUtils.isEmpty(nodName) || TextUtils.isEmpty(nodName.trim())) {
+              et_nodeName.setError(getString(R.string.err_invalidNodeName));
+              return;
             }
+            nodName = nodName.trim();
+            PeerDetails.getInstance().setPeerAddress(nodName);
+            startActivity(new Intent(StartActivity.this, MainActivity.class));
+          }
         });
+  }
+
+  @Override
+  public void onRequestPermissionsResult(
+      int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    if (requestCode == LOCATION_PERMISSIONS_REQUEST_CODE) {
+
+      // Checking whether user granted the permissions or not.
+      if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+        // Showing the toast message
+        Toast.makeText(StartActivity.this, "Location Permissions Granted", Toast.LENGTH_SHORT)
+            .show();
+      } else {
+        Toast.makeText(StartActivity.this, "Location Permissions Denied", Toast.LENGTH_SHORT)
+            .show();
+      }
+    } else if (requestCode == BLUETOOTH_PERMISSIONS_REQUEST_CODE) {
+      if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        Toast.makeText(StartActivity.this, "Bluetooth Permissions Granted", Toast.LENGTH_SHORT)
+            .show();
+      } else {
+        Toast.makeText(StartActivity.this, "Bluetooth Permissions Denied", Toast.LENGTH_SHORT)
+            .show();
+      }
+    } else if (requestCode == RECORD_AUDIO_PERMISSION_REQUEST_CODE) {
+      if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        Toast.makeText(StartActivity.this, "Record Audio Permission Granted", Toast.LENGTH_SHORT)
+            .show();
+      } else {
+        Toast.makeText(StartActivity.this, "Record Audio Permission Denied", Toast.LENGTH_SHORT)
+            .show();
+      }
+    }
+  }
+
+  public static String generateNodeName(int length) {
+    StringBuilder builder = new StringBuilder();
+    SecureRandom random = new SecureRandom();
+
+    for (int i = 0; i < length; i++) {
+      int index = random.nextInt(ALPHA_NUMERIC_STRING.length());
+      builder.append(ALPHA_NUMERIC_STRING.charAt(index));
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == LOCATION_PERMISSIONS_REQUEST_CODE) {
-
-            // Checking whether user granted the permissions or not.
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                // Showing the toast message
-                Toast.makeText(StartActivity.this, "Location Permissions Granted", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(StartActivity.this, "Location Permissions Denied", Toast.LENGTH_SHORT).show();
-            }
-        } else if (requestCode == BLUETOOTH_PERMISSIONS_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(StartActivity.this, "Bluetooth Permissions Granted", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(StartActivity.this, "Bluetooth Permissions Denied", Toast.LENGTH_SHORT).show();
-            }
-        } else if (requestCode == RECORD_AUDIO_PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(StartActivity.this, "Record Audio Permission Granted", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(StartActivity.this, "Record Audio Permission Denied", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    public static String generateNodeName(int length) {
-        StringBuilder builder = new StringBuilder();
-        SecureRandom random = new SecureRandom();
-
-        for (int i = 0; i < length; i++) {
-            int index = random.nextInt(ALPHA_NUMERIC_STRING.length());
-            builder.append(ALPHA_NUMERIC_STRING.charAt(index));
-        }
-
-        return builder.toString();
-    }
+    return builder.toString();
+  }
 }
