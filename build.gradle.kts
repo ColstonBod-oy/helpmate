813d74e559dc125398fa19fc161b022f910bed92
Colston <bodoycolstond@gmail.com>
2024-07-10 17:50:13 +0800

    added init.gradle.kts for spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,40 +2,4 @@
 plugins {
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("com.diffplug.spotless") version "7.0.0.BETA1" apply false
-}
-
-val ktlintVersion = "0.48.1"
-
-subprojects {
-    apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
-    extensions.configure<com.diffplug.gradle.spotless.SpotlessExtension> {
-        kotlin {
-            target("**/*.kt")
-            targetExclude("**/build/**/*.kt")
-            // FIXME: This no longer working after spotless updata
-            // ktlint(ktlintVersion).userData(mapOf("android" to "true"))
-            // Temp Fix
-            ktlint(ktlintVersion)
-            licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
-        }
-        groovy {
-            target("**/*.gradle")
-            targetExclude("**/build/**/*.gradle")
-            // Look for the first line that doesn't have a block comment (assumed to be the license)
-            licenseHeaderFile(rootProject.file("spotless/copyright.gradle"), "(^(?![\\/ ]\\*).*$)")
-        }
-        format("kts") {
-            target("**/*.kts")
-            targetExclude("**/build/**/*.kts")
-            // Look for the first line that doesn't have a block comment (assumed to be the license)
-            licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
-        }
-        format("xml") {
-            target("**/*.xml")
-            targetExclude("**/build/**/*.xml")
-            // Look for the first XML tag that isn't a comment (<!--) or the xml declaration (<?xml)
-            licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
-        }
-    }
 }
