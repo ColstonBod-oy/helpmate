fb7458e2a17e3e03f04b0ae27b94d781641594d2
Colston <bodoycolstond@gmail.com>
2024-07-10 20:25:39 +0800

    added license headers part 1

--- app/src/main/java/com/colston/helpmate/AudioBuffer.java
+++ app/src/main/java/com/colston/helpmate/AudioBuffer.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2023 Colston Bod-oy
+ *
+ * Copyright 2019 Jeremy Walker and contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.colston.helpmate;
 
 /**
@@ -5,38 +22,38 @@ package com.colston.helpmate;
  * {@link android.media.AudioRecord}.
  */
 public abstract class AudioBuffer {
-    private static final int[] POSSIBLE_SAMPLE_RATES =
-            new int[]{8000, 11025, 16000, 22050, 44100, 48000};
+  private static final int[] POSSIBLE_SAMPLE_RATES =
+      new int[] {8000, 11025, 16000, 22050, 44100, 48000};
 
-    final int size;
-    final int sampleRate;
-    final byte[] data;
+  final int size;
+  final int sampleRate;
+  final byte[] data;
 
-    protected AudioBuffer() {
-        int size = -1;
-        int sampleRate = -1;
+  protected AudioBuffer() {
+    int size = -1;
+    int sampleRate = -1;
 
-        // Iterate over all possible sample rates, and try to find the shortest one. The shorter
-        // it is, the faster it'll stream.
-        for (int rate : POSSIBLE_SAMPLE_RATES) {
-            sampleRate = rate;
-            size = getMinBufferSize(sampleRate);
-            if (validSize(size)) {
-                break;
-            }
-        }
-
-        // If none of them were good, then just pick 1kb
-        if (!validSize(size)) {
-            size = 1024;
-        }
-
-        this.size = size;
-        this.sampleRate = sampleRate;
-        data = new byte[size];
+    // Iterate over all possible sample rates, and try to find the shortest one. The shorter
+    // it is, the faster it'll stream.
+    for (int rate : POSSIBLE_SAMPLE_RATES) {
+      sampleRate = rate;
+      size = getMinBufferSize(sampleRate);
+      if (validSize(size)) {
+        break;
+      }
     }
 
-    protected abstract boolean validSize(int size);
+    // If none of them were good, then just pick 1kb
+    if (!validSize(size)) {
+      size = 1024;
+    }
 
-    protected abstract int getMinBufferSize(int sampleRate);
+    this.size = size;
+    this.sampleRate = sampleRate;
+    data = new byte[size];
+  }
+
+  protected abstract boolean validSize(int size);
+
+  protected abstract int getMinBufferSize(int sampleRate);
 }
--- app/src/main/java/com/colston/helpmate/AudioRecorder.java
+++ app/src/main/java/com/colston/helpmate/AudioRecorder.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2023 Colston Bod-oy
+ *
+ * Copyright 2019 Jeremy Walker and contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.colston.helpmate;
 
 import static android.os.Process.THREAD_PRIORITY_AUDIO;
@@ -10,7 +27,6 @@ import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.ParcelFileDescriptor;
 import android.util.Log;
-
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -19,131 +35,124 @@ import java.io.OutputStream;
  * file descriptor will be written to until {@link #stop()} is called.
  */
 public class AudioRecorder {
-    /**
-     * The stream to write to.
-     */
-    private final OutputStream mOutputStream;
+  /** The stream to write to. */
+  private final OutputStream mOutputStream;
 
-    /**
-     * If true, the background thread will continue to loop and record audio. Once false, the thread
-     * will shut down.
-     */
-    private volatile boolean mAlive;
+  /**
+   * If true, the background thread will continue to loop and record audio. Once false, the thread
+   * will shut down.
+   */
+  private volatile boolean mAlive;
 
-    /**
-     * The background thread recording audio for us.
-     */
-    private Thread mThread;
+  /** The background thread recording audio for us. */
+  private Thread mThread;
 
-    /**
-     * A simple audio recorder.
-     *
-     * @param file The output stream of the recording.
-     */
-    public AudioRecorder(ParcelFileDescriptor file) {
-        mOutputStream = new ParcelFileDescriptor.AutoCloseOutputStream(file);
+  /**
+   * A simple audio recorder.
+   *
+   * @param file The output stream of the recording.
+   */
+  public AudioRecorder(ParcelFileDescriptor file) {
+    mOutputStream = new ParcelFileDescriptor.AutoCloseOutputStream(file);
+  }
+
+  /**
+   * @return True if actively recording. False otherwise.
+   */
+  public boolean isRecording() {
+    return mAlive;
+  }
+
+  /** Starts recording audio. */
+  public void start() {
+    if (isRecording()) {
+      Log.w(TAG, "Already running");
+      return;
     }
 
-    /**
-     * @return True if actively recording. False otherwise.
-     */
-    public boolean isRecording() {
-        return mAlive;
+    mAlive = true;
+    mThread =
+        new Thread() {
+          @Override
+          public void run() {
+            setThreadPriority(THREAD_PRIORITY_AUDIO);
+
+            Buffer buffer = new Buffer();
+            @SuppressLint("MissingPermission")
+            AudioRecord record =
+                new AudioRecord(
+                    MediaRecorder.AudioSource.DEFAULT,
+                    buffer.sampleRate,
+                    AudioFormat.CHANNEL_IN_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    buffer.size);
+
+            if (record.getState() != AudioRecord.STATE_INITIALIZED) {
+              Log.w(TAG, "Failed to start recording");
+              mAlive = false;
+              return;
+            }
+
+            record.startRecording();
+
+            // While we're running, we'll read the bytes from the AudioRecord and write them
+            // to our output stream.
+            try {
+              while (isRecording()) {
+                int len = record.read(buffer.data, 0, buffer.size);
+                if (len >= 0 && len <= buffer.size) {
+                  mOutputStream.write(buffer.data, 0, len);
+                  mOutputStream.flush();
+                } else {
+                  Log.w(TAG, "Unexpected length returned: " + len);
+                }
+              }
+            } catch (IOException e) {
+              Log.e(TAG, "Exception with recording stream", e);
+            } finally {
+              stopInternal();
+              try {
+                record.stop();
+              } catch (IllegalStateException e) {
+                Log.e(TAG, "Failed to stop AudioRecord", e);
+              }
+              record.release();
+            }
+          }
+        };
+    mThread.start();
+  }
+
+  private void stopInternal() {
+    mAlive = false;
+    try {
+      mOutputStream.close();
+    } catch (IOException e) {
+      Log.e(TAG, "Failed to close output stream", e);
+    }
+  }
+
+  /** Stops recording audio. */
+  public void stop() {
+    stopInternal();
+    try {
+      mThread.join();
+    } catch (InterruptedException e) {
+      Log.e(TAG, "Interrupted while joining AudioRecorder thread", e);
+      Thread.currentThread().interrupt();
+    }
+  }
+
+  private static class Buffer extends AudioBuffer {
+    @Override
+    protected boolean validSize(int size) {
+      return size != AudioRecord.ERROR && size != AudioRecord.ERROR_BAD_VALUE;
     }
 
-    /**
-     * Starts recording audio.
-     */
-    public void start() {
-        if (isRecording()) {
-            Log.w(TAG, "Already running");
-            return;
-        }
-
-        mAlive = true;
-        mThread =
-                new Thread() {
-                    @Override
-                    public void run() {
-                        setThreadPriority(THREAD_PRIORITY_AUDIO);
-
-                        Buffer buffer = new Buffer();
-                        @SuppressLint("MissingPermission") AudioRecord record =
-                                new AudioRecord(
-                                        MediaRecorder.AudioSource.DEFAULT,
-                                        buffer.sampleRate,
-                                        AudioFormat.CHANNEL_IN_MONO,
-                                        AudioFormat.ENCODING_PCM_16BIT,
-                                        buffer.size);
-
-                        if (record.getState() != AudioRecord.STATE_INITIALIZED) {
-                            Log.w(TAG, "Failed to start recording");
-                            mAlive = false;
-                            return;
-                        }
-
-                        record.startRecording();
-
-                        // While we're running, we'll read the bytes from the AudioRecord and write them
-                        // to our output stream.
-                        try {
-                            while (isRecording()) {
-                                int len = record.read(buffer.data, 0, buffer.size);
-                                if (len >= 0 && len <= buffer.size) {
-                                    mOutputStream.write(buffer.data, 0, len);
-                                    mOutputStream.flush();
-                                } else {
-                                    Log.w(TAG, "Unexpected length returned: " + len);
-                                }
-                            }
-                        } catch (IOException e) {
-                            Log.e(TAG, "Exception with recording stream", e);
-                        } finally {
-                            stopInternal();
-                            try {
-                                record.stop();
-                            } catch (IllegalStateException e) {
-                                Log.e(TAG, "Failed to stop AudioRecord", e);
-                            }
-                            record.release();
-                        }
-                    }
-                };
-        mThread.start();
+    @Override
+    protected int getMinBufferSize(int sampleRate) {
+      return AudioRecord.getMinBufferSize(
+          sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
     }
-
-    private void stopInternal() {
-        mAlive = false;
-        try {
-            mOutputStream.close();
-        } catch (IOException e) {
-            Log.e(TAG, "Failed to close output stream", e);
-        }
-    }
-
-    /**
-     * Stops recording audio.
-     */
-    public void stop() {
-        stopInternal();
-        try {
-            mThread.join();
-        } catch (InterruptedException e) {
-            Log.e(TAG, "Interrupted while joining AudioRecorder thread", e);
-            Thread.currentThread().interrupt();
-        }
-    }
-
-    private static class Buffer extends AudioBuffer {
-        @Override
-        protected boolean validSize(int size) {
-            return size != AudioRecord.ERROR && size != AudioRecord.ERROR_BAD_VALUE;
-        }
-
-        @Override
-        protected int getMinBufferSize(int sampleRate) {
-            return AudioRecord.getMinBufferSize(
-                    sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
-        }
-    }
+  }
 }
